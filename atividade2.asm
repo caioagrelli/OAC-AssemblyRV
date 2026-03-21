f33df27225d4b6a45dc52344fed8d9c9c5fbc2c0
caioagrelli <caiooagrelli@hotmail.com>
2026-03-21 16:40:31 -0300

    fix: replace logic conditionals

--- atividade2.asm
+++ atividade2.asm
@@ -1,18 +1,18 @@
-lw   x9,  a
-lw   x10, b
-lw  x11, m
+lw   x9,  a              # carregar a em x9
+lw   x10, b              # carregar b em x10
+add  x11, x0, x0         # m = 0
 
-beq x10, x11, bloco_soma
-sub x11, x9, x10
-beq x0,  x0, salvar
+blt  x10, x11, sum       # se (b < m), vai para sum
 
-bloco_soma:
-add x11, x9, x10
+sub x11, x9, x10         # caso não faz m = a - b
+beq  x0, x0, end         # pula o sum
 
-salvar:
-sw  x11, m       
- halt
+sum:
+    add x11, x9, x10     # m = a + b
+end:
+    sw   x11, m          # salvar m na memória
+halt
 
-a: .word 0x6  
+a: .word 0x6
 b: .word 0xf
-m: .word 0x0
+m: .word 0x0  
