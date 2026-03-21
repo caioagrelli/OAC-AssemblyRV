130ad8e09e9500974d8c9bbece5e24e53f040e48
caioagrelli <caiooagrelli@hotmail.com>
2026-03-21 16:54:12 -0300

    chore: add coments

--- atividade3.asm
+++ atividade3.asm
@@ -3,15 +3,15 @@ lw  x21, h
 lw  x22, i
 lw  x23, j
 
-beq x22, x23, bloco_if
-sub x19, x20, x21
-beq x0,  x0,  Exit
+beq x22, x23, equal     # caso forem iguais joga pra equal
+sub x19, x20, x21       # caso forem diferentes faz a sub
+beq x0,  x0,  exit      # pula o equal
 
-bloco_if:	
-add x19, x20, x21
+equal:	
+    add x19, x20, x21   # soma (f = g + h)
 
-Exit:
-sw  x19, f
+exit:
+    sw  x19, f          # atribui o resultado de f
 halt
 
 g: .word 0x5   
